996e1cca20517e0d8903620a820671749561f381
RoomeyRahman <roomeyrahman@gmail.com>
2024-04-22 15:27:30 +0600

    add common filters, dbconfig, interceptors and decorator

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { HttpErrorFilter } from './common/filters';
+import { LoggingInterceptor, TransformInterceptor } from './common/interceptor';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { configService } from './common/dbconfig/config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpErrorFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TransformInterceptor,
+    },
+  ],
 })
 export class AppModule {}
