c19d8a976aea7e35ec46c908fa94cc89df525bcb
RoomeyRahman <roomeyrahman@gmail.com>
2024-04-22 16:25:07 +0600

    add employee entity

--- src/employees/entities/employee.entity.ts
+++ src/employees/entities/employee.entity.ts
@@ -1,10 +1,19 @@
-import { Entity, Column, OneToMany } from 'typeorm';
-import { IsNotEmpty, IsEmail } from 'class-validator';
+import { Entity, Column, ManyToOne, TreeChildren, TreeParent } from 'typeorm';
 import { BaseEntity } from '../../common/entities';
+import { Position } from 'src/positions/entities/position.entity';
 
 @Entity({ name: 'employees' })
 export class Employee extends BaseEntity {
 	@Column({ type: 'varchar', length: 200 })
   name: string;
+
+  @ManyToOne(() => Position, position => position.Employees)
+  position: Position;
+
+  @TreeChildren()
+  child: Employee[];
+
+  @TreeParent()
+  parent: Employee;
 }
 
--- src/positions/entities/position.entity.ts
+++ src/positions/entities/position.entity.ts
@@ -1,6 +1,7 @@
 import { Entity, Column, OneToMany } from 'typeorm';
 import { BaseEntity } from '../../common/entities';
 import { Designation } from 'src/common/constant';
+import { Employee } from 'src/employees/entities/employee.entity';
 
 @Entity({ name: 'positions' })
 export class Position extends BaseEntity {
@@ -10,5 +11,8 @@ export class Position extends BaseEntity {
     unique: true
   })
   name: string;
+
+  @OneToMany(() => Employee, Employee => Employee.position)
+  Employees: Employee[];
 }
 
